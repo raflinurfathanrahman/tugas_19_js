605ef03eba217b9792d00ba4c92edf1e36b38244
raflirahman <raflirahman375@gmail.com>
2022-10-04 11:37:02 +0700

    add tugas 19

---
+++ tugas19.js
@@ -0,0 +1,12 @@
+var kata = 'semangat...'
+console.log(kata.repeat(5));
+console.log('=================');
+console.log("*****".repeat(1));
+console.log("*****".repeat(5));
+console.log("*****".repeat(9));
+console.log("*****".repeat(12));
+console.log("*****".repeat(15));
+console.log("*****".repeat(12));
+console.log("*****".repeat(9));
+console.log("*****".repeat(5));
+console.log("*****".repeat(1));
